75d2a799ff3066aed4330c56c40830fa7e9d3dff
Magno <magnoneves2@gmail.com>
2020-10-08 01:10:02 -0300

    UPDATE: Correção na velocidade do scroll

--- src/components/container.js
+++ src/components/container.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import styles from './container.module.scss';
 
 export default ({ children }) => {
     return (
         <div div className="mx-auto" >
             {children}
-            <button type="button" id="btn" className={styles.btnTop}>&#9650;</button>
-            {
-
-                onscroll = function () {
-                    const btn = document.getElementById("btn");
-
-                    btn.addEventListener("click", () => {
-                        document.documentElement.scrollTop = 0;
-                    })
-
-                    if (document.documentElement.scrollTop > 400) {
-                        btn.style.display = "block"
-                    } else {
-                        btn.style.display = "none"
-                    }
-                }
-            }
         </div>
     )
 }
