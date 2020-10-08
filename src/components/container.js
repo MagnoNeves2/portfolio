4b423cb18d0ea464198734c0fb376d18e399c35c
Magno <magnoneves2@gmail.com>
2020-10-07 23:45:45 -0300

    UPDATE: Suavização dos link internos

--- src/components/container.js
+++ src/components/container.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from './container.module.scss';
 
 export default ({ children }) => {
     return (
         <div div className="mx-auto" >
             {children}
+            <button type="button" id="btn" className={styles.btnTop}>&#9650;</button>
+            {
+                window.onscroll = function () {
+                    const btn = document.getElementById("btn");
+
+                    btn.addEventListener("click", () => {
+                        document.documentElement.scrollTop = 0;
+                    })
+
+                    if (document.documentElement.scrollTop > 400) {
+                        btn.style.display = "block"
+                    } else {
+                        btn.style.display = "none"
+                    }
+                }
+            }
         </div>
     )
 }
